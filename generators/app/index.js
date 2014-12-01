35ce699771afd75fca5468fb0b495f2abfca5851
wayou <liuwayong@gmail.com>
2014-12-01 16:56:53 +0800

    init

---
+++ generators/app/index.js
@@ -0,0 +1,9 @@
+var generators = require('yeoman-generator');
+module.exports = generators.Base.extend({
+    method1: function() {
+        console.log('method 1 just ran');
+    },
+    method2: function() {
+        console.log('method 2 just ran');
+    }
+});
