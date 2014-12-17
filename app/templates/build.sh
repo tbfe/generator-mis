56e6c5d87b00566e90f0dfc54117f15606a22a7a
wayou <liuwayong@gmail.com>
2014-12-17 12:02:24 +0800

    replace build.sh with new one

--- app/templates/build.sh
+++ app/templates/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 export PATH=$BUILD_KIT_PATH/git/git-2.0.0/bin:$PATH
+
 if [ -d "_build" ]; then
-	(cd _build && git pull)
+    if [ "$1" = "stable" ]||[ "$1" = "develop" ]||[ "$1" = "master" ];then
+        (cd _build && git checkout "$1" )
+    else
+        (cd _build && git pull)
+    fi
 else
-	git clone http://gitlab.baidu.com/tbfe/build.git _build
+
+    if [ "$1" = "stable" ]||[ "$1" = "develop" ]||[ "$1" = "master" ];then
+        git clone -b "$1" http://gitlab.baidu.com/tbfe/build.git _build
+    else
+        git clone http://gitlab.baidu.com/tbfe/build.git _build
+    fi
 fi
 
-if [ $# != 1 ] || [ $1 != '--skip' ];then
-	(cd _build && sh build_fis.sh $1)
+if [ $# != 1 ] || [ "$1" != '--skip' ];then
+    (cd _build && sh build_fis.sh "$1")
 fi
