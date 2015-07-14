1de00431ce9b1b0f15fad4cde621ce829770e499
langjun <darren.langjun@gmail.com>
2015-07-15 02:19:27 +0800

    suit for the cooder

--- app/templates/deploy-conf.js
+++ app/templates/deploy-conf.js
@@ -3,16 +3,16 @@
  */
 
 module.exports = {
-	//部署机器的HOST
-	'host': '<%= host %>',
-	'modName': '<%= modName %>',
-	'subModName': '<%= subModName %>',
-	//静态端口
-	'staticPort': 8090,
-	//页面访问端口
-	'tplPort': 8080,
-	//静态文件部署路径
-	'staticPath': '/home/work/static/static.tieba.baidu.com',
-	//模版文件部署路径
-	'tplPath': '/home/work/orp/template'
+    // 部署机器的HOST
+    'host': '<%= host %>',
+    'modName': '<%= modName %>',
+    'subModName': '<%= subModName %>',
+    // 静态端口
+    'staticPort': 8090,
+    // 页面访问端口
+    'tplPort': 8080,
+    // 静态文件部署路径
+    'staticPath': '/home/work/static/static.tieba.baidu.com',
+    // 模版文件部署路径
+    'tplPath': '/home/work/orp/template'
 };
